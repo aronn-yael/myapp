205c9bebbfc56bf5c153e02b8868dea89385b760
aronn-yael <aronnwinner@gmail.com>
2025-04-21 11:49:47 +0100

    avec la page home

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('mymodel/', views.my_model_list, name='my_model_list'),
+    # Ajouter la route pour la page d'accueil
+    # Elle sera accessible à l'URL /myapp/
+    path('', views.home_view, name='home'),
 ]
